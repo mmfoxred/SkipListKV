f9c39e32347810033d4e7ae4760199ab4ee3b79f
oucat <tangihua@qq.com>
2023-04-03 16:50:58 +0800

    将Issues问题都解决了,并进行了测试

--- skiplist.h
+++ skiplist.h
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <cstddef>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
@@ -24,12 +25,13 @@ public:
     V    get_value() const;
     void set_value(V v);
     // level
-    Node<K, V>** forward;      // a array to next node
-    int          node_level;   // max level of forward in this node
+    Node<K, V>** forward;   // a array to next node
+    int          get_node_level();
 
 private:
-    K key;
-    V value;
+    K   key;
+    V   value;
+    int node_level;   // max level of forward in this node
 };
 
 // 构造
@@ -51,21 +53,27 @@ Node<K, V>::~Node()
 }
 
 template<typename K, typename V>
-K Node<K, V>::get_key() const
+inline K Node<K, V>::get_key() const
 {
     return this->key;
 }
 
 template<typename K, typename V>
-V Node<K, V>::get_value() const
+inline V Node<K, V>::get_value() const
 {
     return this->value;
 }
 
 template<typename K, typename V>
-void Node<K, V>::set_value(V v)
+inline void Node<K, V>::set_value(V v)
 {
     this->value = v;
+}
+
+template<typename K, typename V>
+inline int Node<K, V>::get_node_level()
+{
+    return this->node_level;
 }
 
 // SkipList
@@ -92,6 +100,7 @@ public:
 private:
     bool is_str_valid(const std::string& str);
     void get_kV_from_string(const std::string& str, std::string& key, std::string& val);
+    void destroy_skiplist(Node<K, V>* node);
 
 private:
     int _max_level;         // maximum level of skiplist
@@ -127,7 +136,13 @@ inline SkipList<K, V>::~SkipList()
     {
         _file_writer.close();
     }
-
+    Node<K, V>* current = this->_header->forward[0];   // 直接来到0层
+    while (current)                                    // 删除kv节点
+    {
+        Node<K, V>* tmp = current->forward[0];
+        delete current;
+        current = tmp;
+    }
     delete this->_header;
 }
 
@@ -165,10 +180,10 @@ int SkipList<K, V>::insert_element(K key, V val)
         return -1;   // -1表示已存在
     }
     // if this key doesn't existed
-    if (current == nullptr || current->get_key() != key)
+    else if (current == nullptr || current->get_key() != key)
     {
         int randLevel = get_randLevel();
-        if (randLevel > _skip_list_level)   //注意这里是_skip_list_level的值
+        if (randLevel > _skip_list_level)   // 注意这里是_skip_list_level的值
         {
             for (int i = _skip_list_level + 1; i < randLevel + 1; i++)
                 update[i] = _header;
@@ -176,9 +191,9 @@ int SkipList<K, V>::insert_element(K key, V val)
         }
         Node<K, V>* node = createNode(key, val, randLevel);
         // insert node
-        //这里要使用randLevel，并不是randLevel ==
+        // 这里要使用randLevel，并不是randLevel ==
         //_skip_list_level，而是在randLevel>_skip_list_level才会有这样的结果,一般还是要取randLevel
-        //为什么下面出错了，定位到上面分配内存的地方？
+        // 为什么下面出错了，定位到上面分配内存的地方？
         for (int i = 0; i <= randLevel; i++)
         {
             node->forward[i]      = update[i]->forward[i];
@@ -216,9 +231,9 @@ void SkipList<K, V>::delete_element(K key)
 {
     mtx.lock();
     Node<K, V>* current = this->_header;
-    Node<K, V>* update[this->_max_level + 1];
+    Node<K, V>* update[_max_level + 1];
     memset(update, 0, sizeof(Node<K, V>*) * (_max_level + 1));
-    //找出所有需要删除的节点
+    // 找出所有需要删除的节点
     for (int i = _skip_list_level; i >= 0; i--)
     {
         while (current->forward[i] != nullptr && current->forward[i]->get_key() < key)
@@ -227,12 +242,13 @@ void SkipList<K, V>::delete_element(K key)
         }
         update[i] = current;
     }
-    //来到0层处
+    // 来到0层处
     current = current->forward[0];
-    //如果这个位置是key，那么删除所有层对应的节点
+    // 如果这个位置是key，那么删除所有层对应的节点
     if (current != nullptr && current->get_key() == key)
     {
-        //删除节点
+        // 删除节点
+        /*
         for (int i = 0; i <= _skip_list_level; i++)
         {
             if (update[i]->forward[i] == current)
@@ -240,11 +256,19 @@ void SkipList<K, V>::delete_element(K key)
             else
                 break;   // 因为是从低到高层遍历的，如果不是了，那么上层也必然不会存在该节点了，可以直接退出
         }
-        //处理个数
+        */
+        // 另一种写法
+        int node_level = current->get_node_level();
+        for (int i = node_level; i >= 0; i--)
+        {
+            update[i]->forward[i] = current->forward[i];
+        }
+        // 处理个数
         while (_skip_list_level >= 0 && _header->forward[_skip_list_level] == nullptr)
         {
             --_skip_list_level;
         }
+        delete current;
         std::cout << "Successfully deleted key " << key << std::endl;
         --_length;
     }
@@ -264,7 +288,7 @@ bool SkipList<K, V>::search_element(K key)
             current = current->forward[i];
         }
     }
-    //来到第0层处
+    // 来到第0层处
     current = current->forward[0];
     if (current && current->get_key() == key)
     {
@@ -317,7 +341,7 @@ void SkipList<K, V>::dump_file()
 template<typename K, typename V>
 inline int SkipList<K, V>::get_randLevel()
 {
-    int k = 0;   //这里要从level == 0开始，不然1层与0层相同
+    int k = 0;   // 这里要从level == 0开始，不然1层与0层相同
     while (rand() % 2)
     {
         ++k;
@@ -349,4 +373,13 @@ inline void SkipList<K, V>::get_kV_from_string(const std::string& str, std::stri
     auto index = str.find(delimit);
     key        = str.substr(0, index);
     val        = str.substr(index + 1, str.length());
+}
+
+template<typename K, typename V>
+void SkipList<K, V>::destroy_skiplist(Node<K, V>* node)
+{
+    if (node == nullptr)
+        return;
+    destroy_skiplist(node->forward[0]);
+    delete node;
 }
